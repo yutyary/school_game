335fda0daee1c6a4353c75cd69494dd04968625a
Yuta Mochizuki <yutyary@gmail.com>
2019-12-14 00:31:58 +0900

    画像の追加、およびクマ画像の移動実装

--- main.js
+++ main.js
@@ -3,12 +3,20 @@ enchant();
 window.onload = function() {
     var core = new Core(320, 320);
     core.preload('chara1.png');
+    core.fps = 15;
     core.onload = function() {
         var bear = new Sprite(32, 32);
         bear.image = core.assets['chara1.png'];
         bear.x = 0;
         bear.y = 0;
         core.rootScene.addChild(bear);
-    };
+
+	bear.addEventListener('enterframe', function() {
+	    this.x += 5;
+	    if (this.x > 320) this.x = 0;
+	});
+
+	core.rootScene.addChild(bear);
+    }
     core.start();
 };
