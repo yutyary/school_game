8f85b99d0d0e2476e512b48c7320b740b0e2568c
Yuta Mochizuki <yutyary@gmail.com>
2019-12-13 23:45:40 +0900

    mainファイルが間違っていたので訂正

--- main.js
+++ main.js
@@ -1,15 +1,14 @@
 enchant();
 
 window.onload = function() {
-	var core = new Core(320,320);
-	core.preload('chara1.png');
-	core.onload = function() {
-		var bear = new Sprite(32,32);
-		bear image = core.assets['chara1.png'];
-		bear.x = 0;
-		bear.y = 0;
-		core.rootScene.addChild(bear);
-	};
-	core.start();
+    var core = new Core(320, 320);
+    core.preload('chara1.png');
+    core.onload = function() {
+        var bear = new Sprite(32, 32);
+        bear.image = core.assets['chara1.png'];
+        bear.x = 0;
+        bear.y = 0;
+        core.rootScene.addChild(bear);
+    };
+    core.start();
 };
-
