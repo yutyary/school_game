3a5a2c49a14ea99cc293542cd319a2d610349ad3
Yuta Mochizuki <yutyary@gmail.com>
2019-12-19 02:26:03 +0900

    移動可能範囲の設定、背景色の変更

--- main.js
+++ main.js
@@ -4,22 +4,31 @@ window.onload = function() {
     var core = new Core(320, 320);
     core.preload('chara1.png');
     core.fps = 15;
+    core.rootScene.backgroundColor = "blue";
     core.onload = function() {
         var bear = new Sprite(32, 32);
         bear.image = core.assets['chara1.png'];
-        bear.x = 0;
-        bear.y = 0;
+        bear.x = 144;
+        bear.y = 144;
 	bear.frame = 1;
 
 	bear.addEventListener('enterframe', function() {
-	    if (core.input.left) this.x -= 5;
-	    if (core.input.right) this.x += 5;
-	    if (core.input.up) this.y -= 5;
-	    if (core.input.down) this.y += 5;
+	    if (core.input.left) {
+		if(bear.x > 0) this.x -= 5;
+	    }
+	    if (core.input.right) {
+		if(bear.x < 280) this.x += 5;
+	    }
+	    if (core.input.up) {
+		if(this.y > 0) this.y -= 5;
+	    }
+	    if (core.input.down) {
+		if(this.y < 280) this.y += 5;
+	    }
 	});
 
 	var label = new Label();
-	label.x = 280;
+	label.x = 275;
 	label.y = 5;
 	label.color = 'red';
 	label.font = '14px "Arial"';
